88f0c45bdf0ef643a29434e01a7125117b196440
mhmd <mhmd.merhie@gmail.com>
2021-08-04 16:30:42 +0300

    MOD

--- react/update_locale.php
+++ react/update_locale.php
@@ -1,12 +1,17 @@
-<?php
+<?php                                                                                                       
 
 $project_id = '3';
 
 $locale_file = __DIR__ . '/../../../locale/locale.json';
 
+$locale = @file_get_contents($locale_file);
+$locale = @json_decode($locale, true);
+
 $endpoint = 'http://tarjim.io/translationkeys/json/full/'.$project_id;
 $result = file_get_contents($endpoint);
 $api_result = json_decode($result, true);
 
-file_put_contents($locale_file, json_encode($api_result));
-echo "Locale file updated" . PHP_EOL;
+if (empty($locale) || $locale['meta']['results_last_update'] < $api_result['meta']['results_last_update']) {
+  file_put_contents($locale_file, json_encode($api_result));
+  echo "Locale file updated" . PHP_EOL;
+}
